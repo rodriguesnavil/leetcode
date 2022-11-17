86cfadf34fb4141efd92faf8e2ab20adf781e907
Navil Rodrigues <32533176+rodriguesnavil@users.noreply.github.com>
2022-11-17 21:01:58 +0530

    Time: 0 ms (100.00%), Space: 2.6 MB (100.00%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.go
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.go
@@ -1,23 +1,7 @@
-func sum(s []int, ch chan int) {
-    total := 0
-    for i := 0; i < len(s); i++ {
-        total += s[i]
-    }
-    ch <- total // Send total to channel ch.
-} 
-
 func runningSum(nums []int) []int {
-    result := [] int{}
-    
-    ch := make(chan int)
-    
-    for i := 0; i < len(nums); i++ {
-        go sum(nums[:i+1], ch)
-        result = append(result, <-ch) // append ch value to result array
+//     optimized solution
+    for i := 1; i < len(nums); i++ {
+        nums[i] = nums[i] + nums[i -1]
     }
-    
-    close(ch) // close channel ch
-    
-    return result
-    
+    return nums
 }
