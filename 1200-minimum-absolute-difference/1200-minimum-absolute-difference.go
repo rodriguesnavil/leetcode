3c08bb14f8d9e29f8f51fbdb39c3b30ff9ea0b16
Navil Rodrigues <32533176+rodriguesnavil@users.noreply.github.com>
2022-11-18 22:40:43 +0530

    Time: 198 ms (11.32%), Space: 9.4 MB (15.09%) - LeetHub

---
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.go
@@ -0,0 +1,26 @@
+func minimumAbsDifference(arr []int) [][]int {
+    if len(arr) < 2 {
+        return [][] int{}
+    }
+    
+    var result [][]int
+    min := math.MaxInt
+    
+    sort.Slice(arr, func(i, j int) bool {
+        return arr[i] < arr[j]
+    })
+    
+    for i := 1; i < len(arr); i++ {
+        if arr[i] - arr[i-1] < min {
+            min = arr[i] - arr[i-1]
+        }
+    }
+    
+    for i := 1; i < len(arr); i++ {
+        if arr[i] - arr[i-1] == min {
+            result = append(result, []int{arr[i-1], arr[i]})
+        }
+    }
+    
+    return result
+}
