e19c6fe8b797eefd2e2fd9d6a0b42652c2ca5753
Navil Rodrigues <32533176+rodriguesnavil@users.noreply.github.com>
2022-12-22 20:22:43 +0530

    Time: 22 ms (90.95%), Space: 6.6 MB (74.57%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.go
+++ 0283-move-zeroes/0283-move-zeroes.go
@@ -1,18 +1,20 @@
 func moveZeroes(nums []int)  {
-    j := 0
+    i, j := 0, 0
     
-    for i := 0; i < len(nums); i++ {
-        if((Abs(nums[i])) > 0) {
+    for i < len(nums) {
+        if findAbs(nums[i]) > 0 {
             nums[i], nums[j] = nums[j], nums[i]
             j++
         }
+        i++
     }
 }
 
 
-func Abs(num int) int {
-    if (num < 0) {
-        return -num
+func findAbs(num int) int {
+    if num > 0 {
+        return num
+    } else {
+        return - num
     }
-    return num
 }
